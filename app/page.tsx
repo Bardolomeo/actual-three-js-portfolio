cea4fb85fcd3614c55a59d0bb91e00de32eaaa96
Bardolomeo <sapiobard@gmail.com>
2024-09-24 20:31:52 +0200

    guuhj

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <main className='max-w-7xl mx-auto'>
+        <h1 className="text-2xl underline text-white">Hello World</h1>
+      </main>
+  );
+}
