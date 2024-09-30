b45aff421e70179f80d5b06f308b163637013156
Bardolomeo <sapiobard@gmail.com>
2024-09-30 19:55:18 +0200

    AGgiunte animazioni camera e qualcosa di luci

--- components/CanvasIndex.tsx
+++ components/CanvasIndex.tsx
@@ -1,69 +1,164 @@
-import { Canvas } from '@react-three/fiber'
-import React, { Suspense, useEffect, useRef, useState } from 'react'
-import VendingMachine from './models/VendingMachine';
-import { Reflector, ReflectorProps } from '@react-three/drei';
-import PsxTv from './models/PsxTv';
-import CanvasLoader from './CanvasLoader';
-import { gifs } from '@/constants';
-import * as THREE from 'three';
+import { Canvas } from "@react-three/fiber";
+import React, { Suspense, useEffect, useState } from "react";
+import VendingMachine from "./models/VendingMachine";
+// import { Reflector, ReflectorProps } from '@react-three/drei';
+import PsxTv from "./models/PsxTv";
+import CanvasLoader from "./CanvasLoader";
+import { gifs } from "@/constants";
+import * as THREE from "three";
+import { getProject } from "@theatre/core";
+import { PerspectiveCamera, SheetProvider } from "@theatre/r3f";
+// import extension from '@theatre/r3f/dist/extension';
+// import studio from '@theatre/studio';
+import cameraAnimations from "@/constants/animations.json";
+import { FiArrowDown, FiArrowUp, FiArrowUpLeft } from "react-icons/fi";
 
-
-const props: ReflectorProps = {
-	blur: [512, 512], // Blur ground reflections (width, heigt), 0 skips blur
-	mixBlur:0.75, // How much blur mixes with surface roughness
-	mixStrength: 1, // Strength of the reflections
-	resolution: 1024, // Off-buffer resolution, lower=faster, higher=better quality
-	rotation: [-Math.PI * 0.5, 0, 0],
-	mirror: 1,// Mirror environment, 0 = texture colors, 1 = pick up env colors
-	minDepthThreshold: 0.25,
-	maxDepthThreshold: 1,
-	depthScale: 50,
-	position: [0, -2.9, 0],
-	mixContrast: 1,
-}
+// const props: ReflectorProps = {
+// 	blur: [512, 512], // Blur ground reflections (width, heigt), 0 skips blur
+// 	mixBlur:0.75, // How much blur mixes with surface roughness
+// 	mixStrength: 1, // Strength of the reflections
+// 	resolution: 1024, // Off-buffer resolution, lower=faster, higher=better quality
+// 	rotation: [-Math.PI * 0.5, 0, 0],
+// 	mirror: 1,// Mirror environment, 0 = texture colors, 1 = pick up env colors
+// 	minDepthThreshold: 0.25,
+// 	maxDepthThreshold: 1,
+// 	depthScale: 50,
+// 	position: [0, -2.9, 0],
+// 	mixContrast: 1,
+// }
 
 const CanvasIndex = () => {
-	const camera = new THREE.PerspectiveCamera();
-	camera.position.z = 30;
-	camera.position.y = 8;
-	camera.rotation.x = -0.3;
-	
-	const [isMobile, setIsMobile] = useState(true);
+  const camera = new THREE.PerspectiveCamera();
+  camera.position.z = 30;
+  camera.position.y = 8;
+  camera.rotation.x = -0.7;
 
-	const tvNumberRef = useRef<number>(null!); 
-	tvNumberRef.current = 0;
+  const [isMobile, setIsMobile] = useState(true);
+  const [tvIdx, setTvIdx] = useState(0);
 
-	useEffect(() => {
-		if (window.innerWidth > 800)
-			setIsMobile(false);
-	}, []);
+  useEffect(() => {
+    if (window.innerWidth > 800) setIsMobile(false);
+  });
 
-	function setCameraTarget(position: THREE.Vector3, idx: number)
-	{
-		tvNumberRef.current = idx + 1;
-		camera.position.x = position.x + (position.x /2);
-		camera.position.y = position.y + 4.5;
-		camera.position.z = position.z + 7;
-	}
+  //theater
+  const cameraSheetUp = getProject("Camera Animation", {
+    state: cameraAnimations,
+  }).sheet("cameraUp");
+  const cameraSheetRight = getProject("Camera Animation", {
+    state: cameraAnimations,
+  }).sheet("cameraRight");
+  const cameraSheetLeft = getProject("Camera Animation", {
+    state: cameraAnimations,
+  }).sheet("cameraLeft");
 
-	return (
-		<div className={`flex flex-col justify-center items-center w-full h-full absolute inset-0 pt-28`}>
-		<Canvas camera={camera} className='w-full h-full'>
-			<Suspense fallback={<CanvasLoader />}>
-				<ambientLight intensity={1}/>
-				{gifs.map(({src, position, rotation, scale}, idx) => (
-					<PsxTv key={idx} scale={scale} rotation={rotation} position={position} src={src} onclick={() => {setCameraTarget(gifs[idx].position, idx)}} mode={tvNumberRef}/>
-				))}
-				{!isMobile && <VendingMachine scale={2} position={[-9,-2,3]} rotation={[0,1,0,'XYZ']}/>}
-				<Reflector {...props} args={[1920, 1080]}>
+  //change camera onClick
+  function setCameraTarget(idx: number) {
+    if (tvIdx === 0) {
+      setTvIdx(idx + 1);
+      if (idx === 0)
+        cameraSheetUp.sequence.play().then(() => setTvIdx(idx + 1));
+      if (idx === 1)
+        cameraSheetRight.sequence.play().then(() => setTvIdx(idx + 1));
+      if (idx === 2)
+        cameraSheetLeft.sequence.play().then(() => setTvIdx(idx + 1));
+    }
+  }
+
+  // TO REMOVE
+  // studio.initialize();
+  // studio.extend(extension);
+
+  function AnimatedCameras() {
+    return (
+      <>
+        <SheetProvider sheet={cameraSheetUp}>
+          <PerspectiveCamera
+            theatreKey="null"
+            makeDefault={tvIdx === 0}
+            position={[0, 13, 30]}
+            rotation={[-0.3, 0, 0]}
+          />
+          <PerspectiveCamera
+            theatreKey="camera"
+            makeDefault={tvIdx === 1}
+            position={[0, 13, 30]}
+          />
+        </SheetProvider>
+        <SheetProvider sheet={cameraSheetRight}>
+          <PerspectiveCamera
+            theatreKey="camera"
+            makeDefault={tvIdx === 2}
+            position={[0, 13, 30]}
+          />
+        </SheetProvider>
+        <SheetProvider sheet={cameraSheetLeft}>
+          <PerspectiveCamera
+            theatreKey="camera"
+            makeDefault={tvIdx === 3}
+            position={[0, 13, 30]}
+          />
+        </SheetProvider>
+      </>
+    );
+  }
+
+  function cameraReset() {
+    if (tvIdx != 0) {
+      if (tvIdx === 1)
+        cameraSheetUp.sequence
+          .play({ direction: "reverse" })
+          .then(() => setTvIdx(0));
+      if (tvIdx === 2)
+        cameraSheetRight.sequence
+          .play({ direction: "reverse" })
+          .then(() => setTvIdx(0));
+      if (tvIdx === 3)
+        cameraSheetLeft.sequence
+          .play({ direction: "reverse" })
+          .then(() => setTvIdx(0));
+    }
+  }
+
+  return (
+    <div
+      className={`flex flex-col justify-center items-center w-full h-full absolute inset-0 pt-28`}
+    >
+      <Canvas className="w-full h-full" gl={{ preserveDrawingBuffer: true }}>
+        <Suspense fallback={<CanvasLoader />}>
+          <AnimatedCameras />
+          <ambientLight intensity={1} />
+          {gifs.map(({ src, position, rotation, scale }, idx) => (
+            <group key={idx}>
+              <PsxTv
+                idx={idx}
+                scale={scale}
+                rotation={rotation}
+                position={position}
+                src={src}
+                onclick={() => {
+                  setCameraTarget(idx);
+                }}
+                mode={tvIdx}
+                mobile={isMobile}
+              />
+            </group>
+          ))}
+          {/* {!isMobile && <VendingMachine scale={2} position={[-3,0,3]} rotation={[0,1,0,'XYZ']}/>} */}
+          {/* <Reflector {...props} args={[1920, 1080]}>
 					{(Material, props) => (
-						<Material metalness={0.5} roughness={1}  {...props} />
+						<Material metalness={1} roughness={1}  {...props} />
 					)}
-				</Reflector>
-			</Suspense>
-		</Canvas>
-	</div>
-	)
-}
+				</Reflector> */}
+        </Suspense>
+      </Canvas>
+      {tvIdx != 0 && (
+        <FiArrowUp
+          className="absolute text-neutral-300 text-7xl border-2 border-neutral-300 rounded-full shadow-md bottom-10"
+          onClick={cameraReset}
+        />
+      )}
+    </div>
+  );
+};
 
-export default CanvasIndex
+export default CanvasIndex;
--- components/models/PsxTv.tsx
+++ components/models/PsxTv.tsx
@@ -1,103 +1,174 @@
 "use client";
-import * as THREE from 'three'
-import React, { MutableRefObject, Ref, RefObject, useRef, useState } from 'react'
-import { Image, useGLTF } from '@react-three/drei'
-import { GLTF } from 'three-stdlib'
-import { ThreeEvent, useFrame, useLoader } from '@react-three/fiber'
-import { PlainAnimator } from "three-plain-animator/lib/plain-animator"
-
+import * as THREE from "three";
+import React, {
+  MutableRefObject,
+  Ref,
+  RefObject,
+  useRef,
+  useState,
+} from "react";
+import { Image, useGLTF } from "@react-three/drei";
+import { GLTF } from "three-stdlib";
+import { ThreeEvent, useFrame, useLoader } from "@react-three/fiber";
+import { PlainAnimator } from "three-plain-animator/lib/plain-animator";
+import { SheetProvider } from "@theatre/r3f";
 
 type GLTFResult = GLTF & {
   nodes: {
-    Object_4: THREE.Mesh
-  }
+    Object_4: THREE.Mesh;
+  };
   materials: {
-    M_PSX_TV: THREE.MeshStandardMaterial
-  }
-}
-
-export interface tvType{
-  scale: number; 
-  position: THREE.Vector3; 
-  rotation: THREE.Euler; 
-  src: string;
-  onclick?: (event: ThreeEvent<MouseEvent>) => void;
-  mode?: MutableRefObject<number>;
+    M_PSX_TV: THREE.MeshStandardMaterial;
+  };
 };
 
-export default function PsxTv({scale, position, rotation, src, onclick, mode} : tvType) {
-  
-  function StaticAnimation({textureSrc, iconPosition, iconSize}: { textureSrc: string; iconPosition: THREE.Vector3; iconSize: [number?, number?, number?]; })
-  {
+export interface tvType {
+  scale: number;
+  position: THREE.Vector3;
+  rotation: THREE.Euler;
+  src: string;
+  onclick?: (event: ThreeEvent<MouseEvent>) => void;
+  mode?: number;
+  mobile?: boolean;
+  idx?: number;
+}
+
+export default function PsxTv({
+  scale,
+  position,
+  rotation,
+  src,
+  onclick,
+  mode,
+  mobile,
+  idx
+}: tvType) {
+  function StaticAnimation({
+    textureSrc,
+    iconPosition,
+    iconSize,
+  }: {
+    textureSrc: string;
+    iconPosition: THREE.Vector3;
+    iconSize: [number?, number?, number?];
+  }) {
     const spriteTexture = useLoader(THREE.TextureLoader, textureSrc);
-    const [animator] = useState(() => new PlainAnimator(spriteTexture, 4, 4, 10,20)); 
-    useFrame(() => animator.animate())
+    const [animator] = useState(
+      () => new PlainAnimator(spriteTexture, 4, 4, 10, 20)
+    );
+    useFrame(() => animator.animate());
     return (
-      <mesh position={iconPosition} rotation={[0,0,1.5707963268, 'XYZ']}>
+      <mesh position={iconPosition} rotation={[0, 0, 1.5707963268, "XYZ"]}>
         <boxGeometry args={iconSize} />
         <meshStandardMaterial map={spriteTexture} transparent={true} />
       </mesh>
-    )
+    );
   }
-  
-  function GifAnimation({textureSrc, iconPosition, iconSize}: { textureSrc: string; iconPosition: THREE.Vector3; iconSize: [number?, number?, number?]; })
-  {
+
+  function GifAnimation({
+    textureSrc,
+    iconPosition,
+    iconSize,
+  }: {
+    textureSrc: string;
+    iconPosition: THREE.Vector3;
+    iconSize: [number?, number?, number?];
+  }) {
     const spriteTexture = useLoader(THREE.TextureLoader, textureSrc);
-    const [animator] = useState(() => new PlainAnimator(spriteTexture, 11, 12, 125, 25)); 
-    useFrame(() => animator.animate())
+    const [animator] = useState(
+      () => new PlainAnimator(spriteTexture, 11, 12, 125, 25)
+    );
+    useFrame(() => animator.animate());
     return (
-      <mesh position={iconPosition} rotation={[0,0,0, 'XYZ']}>
+      <mesh position={iconPosition} rotation={[0, 0, 0, "XYZ"]}>
         <boxGeometry args={iconSize} />
-        <meshStandardMaterial map={spriteTexture} transparent={true}/>
+        <meshStandardMaterial map={spriteTexture} transparent={true} />
       </mesh>
-    )
+    );
   }
-  
+
   const [doTilt, setDoTilt] = useState(false);
   const [isHover, setIsHover] = useState(false);
+  const [colorState, setColorState] = useState(new THREE.Color('#4D96FF'));
+  const [intensityState, setIntensity] = useState(1);
 
-  function toggleHoverOver(){
-    setIsHover(true);
-    setDoTilt(true);
-  }
-
-  function toggleHoverOut(){
-    setIsHover(false);
-  }
-
-  const { nodes, materials } = useGLTF('/psx_old_tv/scene.gltf') as GLTFResult
+  const { nodes, materials } = useGLTF("/psx_old_tv/scene.gltf") as GLTFResult;
   const tvRef = useRef<THREE.Mesh>(null!);
 
   useFrame(() => {
-    if (doTilt && mode?.current === 0){
-      tvRef.current.rotateZ(-0.2)
-      setTimeout(() => {tvRef.current.rotateZ(0.2)}, 50)
+    if (doTilt && mode === 0) {
+      tvRef.current.rotateZ(-0.2);
+      setTimeout(() => {
+        tvRef.current.rotateZ(0.2);
+      }, 50);
       setDoTilt(false);
     }
-  })
+  });
+
+  function onHover()
+  {
+    if (mode != 0)
+      return; 
+    setIsHover(true); 
+    if (idx === 0)
+    {
+      setColorState(new THREE.Color('#FFD93D'));
+    }
+    if (idx === 1)
+      setColorState(new THREE.Color('#FF6B6B'));
+    if (idx === 2)
+      setColorState(new THREE.Color('#6BCB77'));
+    setIntensity(3);
+  }
+
+  function outHover()
+  {
+    setIsHover(false);
+    setColorState(new THREE.Color('#4D96FF'));
+    setIntensity(1);
+  }
 
   return (
     <group scale={scale} position={position} rotation={rotation} dispose={null}>
+      <directionalLight intensity={intensityState} color={colorState}/>
       <mesh
+        onPointerEnter={() => onHover()}
+        onPointerLeave={() => outHover()}
         ref={tvRef}
         geometry={nodes.Object_4.geometry}
         material={materials.M_PSX_TV}
         position={[0, 0.251, 0]}
-        onPointerOver={toggleHoverOver}
-        onPointerOut={toggleHoverOut}
         scale={1}
         onClick={onclick}
       >
-          <StaticAnimation textureSrc='/static_sheet.png' iconPosition={new THREE.Vector3(0, 0.05, ((!isHover && mode?.current === 0) ? 0.25 : 0.22))}
-          iconSize={[0.5, 0.55, 0.005]}/>
-          <GifAnimation textureSrc={src} iconPosition={new THREE.Vector3(0, 0.05, (mode?.current === 0 ? 0.24 : 0.22))}
-          iconSize={[0.55, 0.5, 0.005]}/>
-          <Image url='/shadow-gun.jpeg' scale={0.52} position={new THREE.Vector3(0,0,( (mode?.current === 1) ? 0 : 0.24))}/>
-          <Image url='/shadow-edge.png' scale={0.52} position={new THREE.Vector3(0,0,( (mode?.current === 2) ? 0 : 0.24))}/>
-          <Image url='/shadow-old-tv.png' scale={0.52} position={new THREE.Vector3(0,0,((mode?.current === 3) ? 0 : 0.24))}/>
+        <StaticAnimation
+          textureSrc="/static_sheet.png"
+          iconPosition={new THREE.Vector3(0, 0.05, 0.24)}
+          iconSize={(!isHover && mode === 0 && !mobile) ? [0.5, 0.55, 0.005] : [0, 0, 0]}
+        />
+        <GifAnimation
+          textureSrc={src}
+          iconPosition={new THREE.Vector3(0, 0.05, 0.23)}
+          iconSize={mode === 0 ? [0.55, 0.5, 0.005] : [0, 0, 0]}
+        />
+        <Image
+          url="/shadow-gun.jpeg"
+          scale={mode === 1 ? 0.55 : 0}
+          position={new THREE.Vector3(0, 0.03, 0.24)}
+        />
+        <Image
+          url="/shadow-edge.png"
+          scale={mode === 2 ? 0.52 : 0}
+          position={new THREE.Vector3(0, 0, 0.24)}
+        />
+        <Image
+          url="/shadow-old-tv.png"
+          scale={mode === 3 ? 0.54 : 0}
+          position={new THREE.Vector3(0, 0.043, 0.24)}
+        />
       </mesh>
     </group>
-  )
+  );
 }
 
-useGLTF.preload('/psx_old_tv/scene.gltf')
+useGLTF.preload("/psx_old_tv/scene.gltf");
